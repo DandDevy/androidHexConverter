623a61cfbf0f401a1c2f98b2d45159d642e1ebde
Daniel Ashcroft <dashcroft4@gmail.com>
2019-10-30 10:48:29 +0000

    java doced InputValidation

--- app/src/main/java/com/college_project/hexconverter/controllers/InputValidation.java
+++ app/src/main/java/com/college_project/hexconverter/controllers/InputValidation.java
@@ -1,7 +1,15 @@
 package com.college_project.hexconverter.controllers;
 
+/**
+ * <h1>InputValidation controller</h1>
+ */
 public class InputValidation {
 
+    /**
+     * <p>validates input for the convertDecToHex in the MainActivity before creating Intent.</p>
+     * @param intValueAsString
+     * @return
+     */
     public static boolean isStringConvertibleToHex(String intValueAsString) {
 
         boolean res = true;
