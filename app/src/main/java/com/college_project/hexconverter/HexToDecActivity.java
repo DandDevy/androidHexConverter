bcefefd80ef39fbfcbc2a5d063a518383048b648
Daniel Ashcroft <dashcroft4@gmail.com>
2019-10-30 11:32:29 +0000

    swap back and forth between the 2 converters

--- app/src/main/java/com/college_project/hexconverter/HexToDecActivity.java
+++ app/src/main/java/com/college_project/hexconverter/HexToDecActivity.java
@@ -2,7 +2,11 @@ package com.college_project.hexconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.college_project.hexconverter.activites.MainActivity;
 
 public class HexToDecActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class HexToDecActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hex_to_dec);
+    }
+
+
+    public void convertHexToDec(View view) {
+    }
+
+
+    /**
+     * <p>swap converters</p>
+     * @param view
+     */
+    public void swap(View view) {
+        Intent MainActivityIntent = new Intent(this, MainActivity.class);
+
+        startActivity(MainActivityIntent);
     }
 }
